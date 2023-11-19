947335759db151d500a46b81c0ea6a2cb4dbc389
Vasiliy G. Stavenko <stavenko@spin.fi>
2023-11-19 15:18:48 +0300

    Return of esc on thumb button

--- keyboard/6x3_3/def/keymap.c
+++ keyboard/6x3_3/def/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO  ,  KC_Q, KC_W, KC_E, KC_R, KC_T,                  KC_Y, KC_U, KC_I,     KC_O,   KC_P,   KC_NO,
       KC_NO  ,  LA_ER, KC_S, KC_D, KC_F, KC_G,                 KC_H, KC_J, KC_K,     KC_L,   LA_EL,  KC_NO,
       KC_LSFT, CTL_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM,  KC_DOT, CTL_SL, KC_RSFT,
-                          KC_NO,   KC_LSFT,  LA_NAV,     LA_SYM , KC_RSFT, LA_ALT
+                          KC_ESC,   KC_LSFT,  LA_NAV,     LA_SYM , KC_RSFT, LA_ALT
 
   ),
 
